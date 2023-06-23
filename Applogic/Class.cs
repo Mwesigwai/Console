2f736f93719498cb516900e2e2683788cc3932d4
Mwesigwa <mwesigwai433@gmail.com>
2023-06-23 18:34:33 +0300

    One time commit with the basic setup

---
+++ Applogic/Class.cs
@@ -0,0 +1,17 @@
+﻿namespace Applogic;
+public class Person
+{
+    public static int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static string FullName()
+    {
+        System.Console.WriteLine("What is your first name?");
+        string fname1 = Console.ReadLine();
+        System.Console.WriteLine("Second name?");
+        string sname2 = Console.ReadLine();
+        return fname1 + " " + sname2;
+    }
+}
---
+++ Proj2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Applogic;
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine(Person.Sum(4,5));
+
+System.Console.WriteLine("Your name " +  Person.FullName());
+System.Console.WriteLine("Continue?");
+string choice = Console.ReadLine();
+if (choice == "yes")
+{
+    System.Console.WriteLine("You are the best. give me a minute");
+}
